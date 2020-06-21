a4adfccec184b28070dc0714551f7c960e264172
Ol Kovalenko <avk013@gmail.com>
2020-06-21 09:37:01 +0300

    Добавьте файлы проекта.

---
+++ redom2tab/Form1.Designer.cs
@@ -0,0 +1,227 @@
+﻿namespace test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tab_ = new System.Windows.Forms.TabControl();
+            this.fileList = new System.Windows.Forms.TabPage();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button3 = new System.Windows.Forms.Button();
+            this.tab_.SuspendLayout();
+            this.fileList.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(209, 374);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(197, 65);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "convert >>>";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(-3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(514, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "обработка файлов XML от поисковых систем ";
+            // 
+            // tab_
+            // 
+            this.tab_.Controls.Add(this.fileList);
+            this.tab_.Controls.Add(this.tabPage1);
+            this.tab_.Controls.Add(this.tabPage2);
+            this.tab_.Location = new System.Drawing.Point(2, 28);
+            this.tab_.Name = "tab_";
+            this.tab_.SelectedIndex = 0;
+            this.tab_.Size = new System.Drawing.Size(634, 330);
+            this.tab_.TabIndex = 3;
+            // 
+            // fileList
+            // 
+            this.fileList.Controls.Add(this.label2);
+            this.fileList.Controls.Add(this.listView1);
+            this.fileList.Location = new System.Drawing.Point(4, 22);
+            this.fileList.Name = "fileList";
+            this.fileList.Padding = new System.Windows.Forms.Padding(3);
+            this.fileList.Size = new System.Drawing.Size(626, 304);
+            this.fileList.TabIndex = 2;
+            this.fileList.Text = "files";
+            this.fileList.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(0, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(225, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "тащи файлы в поле";
+            // 
+            // listView1
+            // 
+            this.listView1.GridLines = true;
+            this.listView1.HideSelection = false;
+            this.listView1.LargeImageList = this.imageList1;
+            this.listView1.Location = new System.Drawing.Point(6, 28);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(614, 270);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.ColumnClick += new System.Windows.Forms.ColumnClickEventHandler(this.listView1_ColumnClick);
+            this.listView1.DragDrop += new System.Windows.Forms.DragEventHandler(this.listView1_DragDrop);
+            this.listView1.DragEnter += new System.Windows.Forms.DragEventHandler(this.listView1_DragEnter);
+            this.listView1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.listView1_MouseClick);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "x.ico");
+            this.imageList1.Images.SetKeyName(1, "xml.ico");
+            this.imageList1.Images.SetKeyName(2, "tar.ico");
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.textBox1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(626, 304);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "text_tab";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(6, 11);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(513, 287);
+            this.textBox1.TabIndex = 3;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.dataGridView1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(626, 304);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "grid_tab";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.dataGridView1.Location = new System.Drawing.Point(6, 6);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(605, 292);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.Location = new System.Drawing.Point(2, 374);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(188, 65);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "v3 file >>>";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.tab_);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "XML_DMARC_decoder";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.Form1_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.Form1_DragEnter);
+            this.tab_.ResumeLayout(false);
+            this.fileList.ResumeLayout(false);
+            this.fileList.PerformLayout();
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TabControl tab_;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TabPage fileList;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ redom2tab/Form1.cs
@@ -0,0 +1,275 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {/*
+            textBox1.ScrollBars = ScrollBars.Both;
+            ////////////////////////////тадагрид
+            int stro = 0;
+            for (int i = 0; i <= 100; i++) dataGridView1.Columns.Add("Column", i.ToString());
+            for (int i = 0; i < 100; i++) dataGridView1.Rows.Add();
+            for (int i = 0; i < 100; i++) dataGridView1.Rows[stro++].HeaderCell.Value = i.ToString();
+            ////////////////////////////
+            int a2, a1 = a2 = 1;
+            //DataTable datas = new string [90,90];
+            //dataGridView1.RowCount = 100;
+            //dataGridView1.ColumnCount = 100;
+            XmlDocument xDoc = new XmlDocument();
+            xDoc.Load("Y:\\source\\repos\\test_\\test\\xml_file_in\\1.xml");
+            // получим корневой элемент
+            //textBox1.Text += xDoc.ToString()+ System.Environment.NewLine;
+            XmlElement xRoot = xDoc.DocumentElement;
+            //textBox1.Text += xRoot.ToString()+System.Environment.NewLine;
+            // обход всех узлов в корневом элементе
+            foreach (XmlNode xnode in xRoot)
+            {
+                a1 = 1;
+                dataGridView1.Rows[a1].Cells[a2++].Value = xnode.FirstChild.InnerText;
+                textBox1.Text += xnode.FirstChild.InnerText + System.Environment.NewLine;
+                if (xnode.HasChildNodes)
+                {
+
+                    foreach (XmlNode xnode1 in xnode)
+                    {
+
+                        textBox1.Text += xnode1.FirstChild.InnerText + System.Environment.NewLine;
+                        //  dataGridView1.Rows[a1++].Cells[a2].Value = xnode1.FirstChild.InnerText;
+                        if (xnode1.HasChildNodes)
+                        {
+
+                            foreach (XmlNode xnode2 in xnode1)
+                            {//a2=a2 + 1;
+                             //  textBox1.Text += xnode2.FirstChild.InnerText + System.Environment.NewLine;
+                             // dataGridView1.Rows[a1].Cells[a2++].Value = xnode2.FirstChild.InnerText;
+                             //dataGridView1.Rows[a1++].Cells[a2].Value = xnode2.FirstChild.InnerText;
+                            }
+                        }
+                    }
+                }
+                // получаем атрибут name
+                /*if (xnode.Attributes.Count > 0)
+                {
+                    XmlNode attr = xnode.Attributes.GetNamedItem("record");
+                   // if (attr != null)
+                      //  textBox1.Text += attr.Value;
+                    //Console.WriteLine(attr.Value);
+                }
+                // обходим все дочерние узлы элемента user
+                foreach (XmlNode childnode in xnode.ChildNodes)
+                {
+                    // если узел - company
+                    if (childnode.Name == "source_ip")
+                        //if (childnode.
+                    {
+                        //Console.WriteLine($"count: {childnode.InnerText}");
+                        textBox1.Text += childnode.InnerText;
+                    }
+                    // если узел age
+                    if (childnode.Name == "domain")
+                    {
+                        //Console.WriteLine($"domain: {childnode.InnerText}");
+                        textBox1.Text += childnode.InnerText;
+                    }
+                */
+           
+     //   }
+    //}
+
+    //    dataGridView1.DataSource = datas;
+    //  Console.WriteLine();
+
+
+    //dataGridView1.DataSource = datas;
+    //dataGridView1.AutoResizeColumns();
+
+}
+        public class feedback
+        {
+            // public string feedback;
+            public string source_ip;
+            public string count;
+            public string dkim;
+            public string spf;
+            public string domain;
+        }
+         public string[] textfromtag(string source, string begino, string finalo)
+        {//ищем содержимое в тегах
+            string[] res = { };
+            begino = "<" + begino + ">";
+            finalo = "<" + finalo + ">";
+            int i = 0;
+            finalo.Replace("/", @"\/");//для Reg маскируем слеш
+            /////////
+            //Regex Reg = new Regex(@"""" + begino + ""(.*?)"" + finalo+"""");
+            System.Text.RegularExpressions.Regex Reg = new Regex(@begino + @"(.*?|(\s?|\S?))" + @finalo);
+           // textBox28.Text = @begino + @"(.*?|(\s|\S))" + @finalo;
+            MatchCollection reHref = Reg.Matches(source);
+
+            foreach (Match match in reHref)
+            {
+                Array.Resize<string>(ref res, i + 1);
+                res[i] = match.ToString();
+                res[i] = res[i].Remove(0, begino.Length);
+                res[i] = res[i].Remove(res[i].Length - finalo.Length, finalo.Length);
+                i++;
+               // textBox7.Text += i.ToString() + "=";
+            }
+            ////////
+            return res;
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string[] res = { };
+            string[] res2 = { };
+            string[] key = {"source_ip","count","dkim","spf","domain","result"  };
+
+            DataTable dt = new DataTable("tab0");//создаем таблицу данных
+            for (int i = 0; i < key.Length; i++)//инициализируем столбцы dt по ключам
+                dt.Columns.Add(key[i]);
+             //string mas1[];
+            //string path = @"C:\SomeDir\hta.txt";
+            //  using (FileStream fs = new FileStream("Y:\\source\\repos\\test_\\test\\xml_file_in\\1.xml", FileMode.Open))
+            //            {                textBox1.Text = textfromtag(fs, "<record>", "<record>");
+            //}
+            string sfull="",path = "Y:\\source\\repos\\test_\\test\\xml_file_in\\1.xml";
+            string[] readText = File.ReadAllLines(path);
+            foreach (string s in readText)
+            {
+                sfull += s;
+                //Console.Read();
+
+             
+                
+            }
+            res = textfromtag(sfull, "record", "/record");
+            for (int j = 0; j < res.Length; j++)//считываем все в record
+            {
+                dt.Rows.Add(); // добавляем столбцы соглано 
+                for (int i = 0; i < key.Length; i++)// разбираем по key
+                {                    res2 = textfromtag(res[j], key[i], "/" + key[i]);
+                    textBox1.Text = textBox1.Text + res2[0];
+                    dt.Rows[j][key[i]] = res2[0];
+                    //  MessageBox.Show(dt.Rows[0][key[i]].ToString());
+                }
+                textBox1.Text += System.Environment.NewLine;
+            }
+              dataGridView1.DataSource = dt;
+        }
+
+        private void Form1_DragDrop(object sender, DragEventArgs e)
+        {
+           
+        }
+        void tutu(DragEventArgs e)
+        {
+            listView1.SmallImageList = imageList1;
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                string[] filePaths = (string[])(e.Data.GetData(DataFormats.FileDrop));
+                foreach (string fileLoc in filePaths)
+                {
+                    // Code to read the contents of the text file
+                    if (File.Exists(fileLoc))
+                    {
+                        // using (TextReader tr = new StreamReader(fileLoc))
+                        {
+                            //   MessageBox.Show(tr.ReadToEnd());
+                           // MessageBox.Show(fileLoc);
+                            ListViewItem lvi = new ListViewItem();
+                            // установка названия файла
+                            lvi.Text = fileLoc.Remove(0, fileLoc.LastIndexOf('\\') + 1);
+                            lvi.Name = fileLoc;
+                            string ext = fileLoc.Remove(0, fileLoc.LastIndexOf('.') + 1);
+                            // MessageBox.Show(ext);
+                            if (ext=="tar") lvi.ImageIndex = 2;
+                            else if (ext=="xml") lvi.ImageIndex = 1;
+                            else lvi.ImageIndex = 0;
+                            //lvi.ImageIndex = 1; // установка картинки для файла
+                                                // добавляем элемент в ListView
+                            listView1.Items.Add(lvi);
+                        }
+                    }
+                }
+            }
+            //MessageBox.Show("+");
+
+        }
+        private void Form1_DragEnter(object sender, DragEventArgs e)
+        {//перетаскивание файла на форму....
+          //  tutu(e);
+            /*listView1.SmallImageList = imageList1;
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                string[] filePaths = (string[])(e.Data.GetData(DataFormats.FileDrop));
+                foreach (string fileLoc in filePaths)
+                {
+                    // Code to read the contents of the text file
+                    if (File.Exists(fileLoc))
+                    {
+                       // using (TextReader tr = new StreamReader(fileLoc))
+                        {
+                            //   MessageBox.Show(tr.ReadToEnd());
+                            MessageBox.Show(fileLoc);
+                            ListViewItem lvi = new ListViewItem();
+                            // установка названия файла
+                            lvi.Text = fileLoc.Remove(0, fileLoc.LastIndexOf('\\') + 1);
+                            lvi.ImageIndex = 1; // установка картинки для файла
+                                                // добавляем элемент в ListView
+                            listView1.Items.Add(lvi);
+                        }
+                    }
+                }
+            }
+            //MessageBox.Show("+");
+            */
+        }
+
+        private void listView1_DragDrop(object sender, DragEventArgs e)
+        {
+          tutu(e);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listView1.AllowDrop = true;
+          //  listView1.DragDrop += new DragEventHandler(listView1_DragDrop);
+           // listView1.DragEnter += new DragEventHandler(listView1_DragEnter);
+        }
+
+        private void listView1_DragEnter(object sender, DragEventArgs e)
+        {
+          e.Effect = DragDropEffects.Copy;
+        }
+
+        private void listView1_ColumnClick(object sender, ColumnClickEventArgs e)
+        {
+                  }
+
+        private void listView1_MouseClick(object sender, MouseEventArgs e)
+        {
+
+            String fullPath = listView1.SelectedItems[0].Name;
+            MessageBox.Show(fullPath);
+        }
+    }
+}
